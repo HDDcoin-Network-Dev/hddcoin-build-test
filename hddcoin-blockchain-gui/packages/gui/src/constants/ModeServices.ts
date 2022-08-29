7b93700c757a95f1f1bad98b591433bd94895514
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2022-08-29 15:19:45 -0500

    Rename GUI
    Create hddcoin-blockchain-gui
    
    remove gui repo

---
+++ hddcoin-blockchain-gui/packages/gui/src/constants/ModeServices.ts
@@ -0,0 +1,20 @@
+import { ServiceName } from '@chia/api';
+import { Mode } from '@chia/core';
+
+export default {
+  [Mode.WALLET]: [
+    ServiceName.WALLET,
+  ],
+  [Mode.FARMING]: [
+    ServiceName.WALLET, 
+    ServiceName.FULL_NODE,
+    ServiceName.FARMER,
+    ServiceName.HARVESTER,
+    // ServiceName.DATALAYER,
+  ],
+};
+
+export const SimulatorServices = [
+  ServiceName.WALLET,
+  ServiceName.SIMULATOR,
+];
